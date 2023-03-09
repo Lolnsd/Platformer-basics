e207695ae9857b1d16efd9d2b0ba78555a1072bd
Nguyen Mierzejewski <dawidmierzejewski03@gmail.com>
2023-03-09 21:59:17 +0100

    Files upload

---
+++ Assets/PAP.cs
@@ -0,0 +1,22 @@
+
+//Player Animation Parameters
+public static class PAP {
+    public const string moveX = "moveX";
+    public const string isMoving = "isMoving";
+
+    public const string axisXinput = "Horizontal";
+
+    public const string jumpKeyName = "Jump";
+
+    public const string forceX = "forceX";
+    public const string impulseX = "impulseX";
+    public const string impulseY = "impulseY";
+    public const string velocityY = "velocityY";
+    public const string isOnGround = "isOnGround";
+    public const string isOnWall = "isOnWall";
+    public const string landedOnGround = "landedOnGround";
+    public const string JumpTriggerName = "jump";
+    public const string hasDoubleJumped = "hasDoubleJumped";
+
+    public const string stopVelocity = "stopVelocity";
+}
